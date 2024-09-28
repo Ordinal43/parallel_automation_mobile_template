5e6f0fa238db07bd259e234cb12e45f4c323c0bd
Bryan Ramaputra <ramaputra.bryan@gmail.com>
2024-09-27 18:05:52 -0700

    [FIX] ensure task 'clean' is run first

--- run_test.sh
+++ run_test.sh
@@ -4,7 +4,7 @@ THREADS=${1:-"1"}
 echo "Run test on $THREADS parallel threads"
 
 if [[ "$THREADS" =~ ^[0-9]+$ ]]; then
-  ./gradlew runTest -Dcourgette.threads="$THREADS"
+  ./gradlew clean runTest -Dcourgette.threads="$THREADS"
   allure generate ./build/allure-results/ -o ./build/allure-report --clean
 else
   echo "Argument must be a positive integer"
